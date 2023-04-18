8543f1c1fc09e11e3ef97fbdf0d836fd019429e3
ngoquangkhoanqkey <ngoquangkhoanqk@gmail.com>
2023-04-18 15:41:56 +0700

    ngSwtitch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,11 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'home', component: HomeComponent },
   { path: '', component: HomeComponent },
-  {path: '**', component:PageNotFoundComponent},
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
