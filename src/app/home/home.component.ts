90c530e421815651e1970ece7f17afd45ddcebbe
ngoquangkhoanqkey <ngoquangkhoanqk@gmail.com>
2023-04-18 10:55:12 +0700

    work with object and ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,14 +9,14 @@ import { empty } from 'rxjs';
 export class HomeComponent implements OnInit{
   public name = 'Khoa';
   public age = 17; 
+
+  public feshfood = ['Tao','Cam'];
+  public feshfood2 = [{name: 'Tao', gia:12, sale: true}, {name: 'Cam', gia:10, sale:false},{name: 'Quyt', gia:-3200, sale:true},{name: 'Thom', gia:3000, sale:false}];
+
+
   constructor(){}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
   }
 
-  public resetName(): void {
-    this.name = '';
-    console.log('name reset');
-    
-  }
 }
